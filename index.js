4918348bfead15bf1829fb4c3e7d5601787ebd89
developer-munir <devolopermunir@gmail.com>
2022-10-25 11:20:21 +0600

    added python and java course data

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
 const Port = process.env.Port || 5000;
+app.use(cors());
+const categories = require('./Data/categories.json');
+app.get('/', (req, res) => {
+    res.send('server is ok');
+})
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
+
 
 app.listen(Port, () => {
     console.log('Server is running ohh!!');
